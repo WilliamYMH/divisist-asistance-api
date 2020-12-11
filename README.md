35277e9fdde9d7ee9895827506bfafc93e390e7d
William <williamyesid.10@gmail.com>
2020-12-10 23:07:27 -0500

    v0.1 delete endpoints unused

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Api endpoint - Divisist Asistance
 
 Proyecto simple que haciendo uso de webscrapping, consulta las notas de las materias de un estudiante.
-Api endpoint:
+- Api endpoint:
 ```
 [POST]
 api/v1.0/get-notas-materias
--- api.py
+++ api.py
@@ -8,33 +8,6 @@ import sys
 def index():
     user_agent = request.headers.get('User-Agent')
     return 'Api web service - divisist asistance'
-
-@app.route('/login', methods=['POST'])
-def login():
-    usuario = request.form.get('usuario')
-    password = request.form.get('password')
-    documento = request.form.get('documento')
-    #documento2 = '*'*len(documento)
-    documento2 =''
-    session = initialize()
-    payload = {
-        'login':'1', 
-        'miip':'',
-        'miipreal':'',
-        'usuario':usuario,
-        'documento2':documento2,
-        'password':password,
-        'documento':documento,
-    }
-    if iniciar_sesion(session, payload):
-        return jsonify(success=True)
-    return jsonify(success=False)
-
-@app.route('/logout', methods=['POST'])
-def logout(session):
-    if cerrar_sesion(session):
-        return jsonify(success=True)
-    return jsonify(success=False)
     
 @app.route('/api/v1.0/get-notas-materias', methods=['POST'])
 def get_notas_materias():
